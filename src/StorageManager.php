d97049e5565385605750c9c860fe4412f12b2aeb
SyntaxWanderer <email1@example.com>
2026-04-04 13:48:04 +0300

    Update StorageManager.php

--- src/StorageManager.php
+++ src/StorageManager.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Semitexa\Storage;
 
-use Semitexa\Core\Attributes\SatisfiesServiceContract;
+use Semitexa\Core\Attribute\SatisfiesServiceContract;
 use Semitexa\Core\Environment;
 use Semitexa\Storage\Contract\StorageDriverInterface;
 use Semitexa\Storage\Contract\StorageObjectStoreInterface;
